7830f5a4a8a56437e7fb8598f4c44a598dc618b4
Dave Snider <dave.snider@gmail.com>
2023-08-31 19:43:35 -0400

    upgrade packages

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="@astro/client" />
+/// <reference types="astro/client" />
 declare global {
   var myString: string;
   function myFunction(): boolean;
 }
 
-export {};
+export { };
