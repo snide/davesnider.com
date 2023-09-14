4312ac3fb35a276da37b927c9dc6ec1a845775f6
Dave Snider <dave.snider@gmail.com>
2023-09-14 12:56:16 -0400

    remove server output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import robotsTxt from 'astro-robots-txt';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davesnider.com',
-  output: 'server',
   integrations: [
     robotsTxt(),
     mdx({
