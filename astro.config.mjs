cca6a76677815d29d143a230f30063b66ce0eaea
Dave Snider <dave.snider@gmail.com>
2023-09-14 12:55:23 -0400

    remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from 'astro/config';
 import theme from './codeTheme.json';
 import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
 import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://davesnider.com',
   output: 'server',
-  adapter: vercel({
-    imageService: true,
-    includeFiles: ['src/images/']
-  }),
-  output: 'hybrid',
   integrations: [
     robotsTxt(),
     mdx({
