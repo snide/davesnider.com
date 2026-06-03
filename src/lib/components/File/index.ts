30a886ab286265cc7fcf97793a012b9a9ad14d0f
Dave Snider <dave.snider@gmail.com>
2026-06-03 07:56:39 -0400

    Add colorband, and db columns for meuseum color

--- src/lib/components/File/index.ts
+++ src/lib/components/File/index.ts
@@ -1,3 +1,4 @@
+export { default as ColorBand } from './ColorBand.svelte';
 export { default as FileCard } from './FileCard.svelte';
 export { default as FileList } from './FileList.svelte';
 export { default as FilesEmbed } from './FilesEmbed.svelte';
